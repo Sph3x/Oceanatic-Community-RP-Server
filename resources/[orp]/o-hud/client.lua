58da23cbe51451cd5fb24ee9e4787c84e263459f
Tomás Antunes <33696484+tsadev@users.noreply.github.com>
2018-04-05 21:51:12 +0100

    some minor changes (i think)

--- resources/[orp]/o-hud/client.lua
+++ resources/[orp]/o-hud/client.lua
@@ -44,3 +44,13 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if GetPlayerWantedLevel(PlayerId()) ~= 0 then
+            SetPlayerWantedLevel(PlayerId(), 0, false)
+            SetPlayerWantedLevelNow(PlayerId(), false)
+        end
+    end
+end)
